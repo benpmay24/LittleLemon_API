5b3fbfac5eec6b4b72ee20c00045c3975f816992
Benjamin May <benmay@tesla.com>
2024-07-29 16:53:47 -0700

    Minor additions

--- LLApi/urls.py
+++ LLApi/urls.py
@@ -2,12 +2,10 @@ from django.urls import include, path
 from rest_framework import routers
 from . import views
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path("groups/manager/users/",views.ManagerGroupViewSet.as_view({'get': 'list', 'post': 'create', 'delete': 'destroy'})),
+    path("groups/delivery-crew/users/",views.ManagerGroupViewSet.as_view({'get': 'list', 'post': 'create', 'delete': 'destroy'})),
     path("secret/",views.secret_message),
     path("users/",include("djoser.urls")),
     path("users/",include("djoser.urls.authtoken")),
--- LLApi/views.py
+++ LLApi/views.py
@@ -1,29 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import Group, User
-from rest_framework import permissions, viewsets
+from rest_framework import permissions, viewsets, status, generics
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from .models import Category, MenuItem, Cart, Order, OrderItem
+from .serializers import CategorySerializer, MenuItemSerializer, CartSerializer, OrderSerializer, UserSerilializer
 
-from LLApi.serializers import GroupSerializer, UserSerializer
+class ManagerGroupViewSet(viewsets.ViewSet):
+    permission_classes = [IsAdminUser]
+    def list(self, request):
+        users = User.objects.all().filter(groups__name='Manager')
+        items = UserSerilializer(users, many=True)
+        return Response(items.data)
 
+    def create(self, request):
+        user = get_object_or_404(User, username=request.data['username'])
+        managers = Group.objects.get(name="Manager")
+        managers.user_set.add(user)
+        return Response({"message": "user added to the manager group"}, 200)
 
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    def destroy(self, request):
+        user = get_object_or_404(User, username=request.data['username'])
+        managers = Group.objects.get(name="Manager")
+        managers.user_set.remove(user)
+        return Response({"message": "user removed from the manager group"}, 200)
+    
+class DeliveryGroupViewSet(viewsets.ViewSet):
+    permission_classes = [IsAuthenticated]
+    def list(self,request):
+        delivery_ppl=User.objects.all().filter(groups__name="Delivery Crew")
+        items=UserSerilializer(delivery_ppl,many=True)
+        return Response(items.data)
+    
+    def create(self,request):
+        person=get_object_or_404(User,username=request.data["username"])
+        delivery_persons=Group.objects.get(name="Delivery Crew")
+        delivery_persons.user_set.add(person)
+        return Response({"message": "user removed from the delivery group"}, 200)
 
+    def destroy(self,request):
+        person=get_object_or_404(User,username=request.data["username"])
+        delivery_persons=Group.objects.get(name="Delivery Crew")
+        delivery_persons.user_set.remove(person)
+        return Response({"message": "user removed from the delivery group"}, 200)
 
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-    queryset = Group.objects.all().order_by('name')
-    serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
 @api_view()
 @permission_classes([IsAuthenticated])
